a02c77453ff37432badfccab18cc58fdf921cacc
luor3 <986982370luo@gmail.com>
2021-11-05 21:07:51 -0500

    Add Job controller

---
+++ database/migrations/2021_10_19_234435_create_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->id();
+            $table->unsignedBigInteger('user');
+            $table->foreign('user')->references('id')->on('users')->onDelete('cascade');
+            $table->text('configuration')->nullable();
+            $table->boolean('status')->default(1);
+            $table->enum('progress', ['Pending', 'In Progress', 'Completed','Cancelled'])->default('Pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
